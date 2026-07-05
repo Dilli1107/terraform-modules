a1c879df8d6c73d32698b6a12b075763d1d49383
Dilli1107 <61258623+Dilli1107@users.noreply.github.com>
2026-07-05 13:25:09 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,9 +1,12 @@
-module "instance_provisioning" {
-  source      = "../module"
-  sgname      = var.sgname
-  cidr        = var.cidr
-  mytag       = var.mytag
-  amiid       = var.amiid
-  machinetype = var.machinetype
-  keyname     = var.keyname
+resource "aws_instance" "JD_Web_server" {
+    ami = var.amiid
+    instance_type = var.machinetype
+    vpc_security_group_ids = ["sg-0313f9f1351c99ac3"]
+    key_name = var.keyname
+
+
+    tags = {
+        Name = var.mytag
+    }
+  
 }
